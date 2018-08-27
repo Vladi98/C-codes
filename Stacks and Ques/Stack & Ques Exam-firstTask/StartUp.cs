92e64c1cea121f18a4be95bfcbbe82e3e27e5554
Vladi98 <37123303+Vladi98@users.noreply.github.com>
2018-08-27 15:46:32 +0300

    Add files via upload

---
+++ Stacks and Ques/Stack & Ques Exam-firstTask/StartUp.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advanced_Exersices
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int sizeOfGunBarrel = int.Parse(Console.ReadLine());
+
+            var listOfBullets = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var listOfLocks = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int money = int.Parse(Console.ReadLine());
+
+            var bullets = new Stack<int>(listOfBullets);
+            var locks = new Queue<int>(listOfLocks);
+            int count = 0;
+
+            var bulletsCount = 0;
+
+            while (true)
+            {
+                count++;
+
+
+                if (bullets.Peek() > locks.Peek())
+                {
+                    Console.WriteLine("Ping!");
+                    
+                    bulletsCount++;
+                    bullets.Pop();
+                }
+
+                else if (bullets.Peek() <= locks.Peek())
+                {
+                    Console.WriteLine("Bang!");
+                    
+                    bulletsCount++;
+                    locks.Dequeue();
+                    bullets.Pop();
+                }
+
+
+                if (count == sizeOfGunBarrel)
+                {
+                    Console.WriteLine("Reloading!");
+                    count = 0;
+                }
+
+                if (locks.Count == 0)
+                {
+                    var moneyResult = money - bulletsCount * bulletPrice;
+
+                    Console.WriteLine($"{bullets.Count} bullets left. Earned ${moneyResult}");
+                    break;
+                }
+
+                else if (bullets.Count == 0)
+                {
+                    Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+                    break;
+                }
+
+                
+                 
+            }
+        }
+    }
+}
---
+++ Stacks and Ques/StackExrecises/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackExrecises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var input = Console.ReadLine();
+
+            var stack = new Stack<char>();
+            
+            var flag = true;
+
+            
+            foreach (char para in input)
+
+            {
+
+                switch (para)
+
+                {
+
+                    case '[':
+
+                    case '(':
+
+                    case '{':
+
+                        stack.Push(para);
+
+                        break;
+                        
+                    case '}':
+
+                        if (!stack.Any())
+
+                            flag = false;
+
+                        
+                        else if (stack.Pop() != '{')
+
+                            flag = false;
+
+                        break;
+                        
+
+                    case ')':
+
+                        if (!stack.Any())
+
+                            flag = false;
+
+
+
+                        else if (stack.Pop() != '(')
+
+                            flag = false;
+
+                        break;
+                        
+
+                    case ']':
+
+                        if (!stack.Any())
+
+                            flag = false;
+
+
+
+                        else if (stack.Pop() != '[')
+
+                            flag = false;
+
+                        break;
+
+                }
+
+
+
+                if (!flag)
+
+                    break;
+
+            }
+            
+            Console.WriteLine(flag ? "YES" : "NO");
+
+
+
+        }
+    }
+}
+
+//5 2 32 
+//1 13 45 32 4
